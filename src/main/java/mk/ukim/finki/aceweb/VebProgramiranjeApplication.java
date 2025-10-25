37e91da031a959cd95a6092d3346eebb2a248e26
Aleksandar Ivanov <aleksandarivanov0907@gmail.com>
2025-10-25 13:11:25 +0200

    initial commit

---
+++ src/main/java/mk/ukim/finki/aceweb/VebProgramiranjeApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.aceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VebProgramiranjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VebProgramiranjeApplication.class, args);
+    }
+
+}
