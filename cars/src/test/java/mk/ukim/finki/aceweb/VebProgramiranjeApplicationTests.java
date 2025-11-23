cbf3e948833bda86ea59b0bdc577421c4341baa4
Aleksandar Ivanov <aleksandarivanov0907@gmail.com>
2025-11-23 23:39:08 +0100

    merged projcets

---
+++ cars/src/test/java/mk/ukim/finki/aceweb/VebProgramiranjeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.aceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebProgramiranjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
